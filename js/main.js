7d3c3e787a81226f0d594d9f9bc18dde841daf87
YehorKanaiev <egor15kanaev@gmail.com>
2021-05-17 22:13:20 +0300

    Adds footer and carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-$(function(){
 
+$(document).ready(function() {
+
+    var owl = $(".front-carousel");
+
+    owl.owlCarousel({
+        singleItem:true,
+        autoPlay: 6000,
+        slideSpeed : 1000,
+        paginationSpeed : 1000,
+        rewindSpeed: 1000,
+        stopOnHover: true,
+    });
+
+    // Custom Navigation Events
+    $(".front-carousel__next").click(function(){
+        owl.trigger('owl.next');
+    })
+    $(".front-carousel__prev").click(function(){
+        owl.trigger('owl.prev');
+    })
 });
